5a36596a57546d5d7c6f8664b9a9450dcb43e4c5
gsadanala <ganesh8374387106@gmail.com>
2024-05-06 14:20:59 -0400

    JDBC based authentication

--- src/main/java/com/ganesh/lil/adminweb/config/WebSecurityConfig.java
+++ src/main/java/com/ganesh/lil/adminweb/config/WebSecurityConfig.java
@@ -8,7 +8,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -25,14 +30,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    @Override
-    public UserDetailsService userDetailsService(){
-        UserDetails user= User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
+    public UserDetailsService users(DataSource dataSource){
+        return new JdbcUserDetailsManager(dataSource);
+    }
 
-        return new InMemoryUserDetailsManager(user);
+    @Bean
+    public static PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
